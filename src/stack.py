a2c4ce35a28069b012cbaff44966f6cc85f2c65a
Дмитрий Дрёмин <dremin.da24108292@gmail.com>
2024-03-25 00:05:13 +0300

    first HW_13_1 data_struct complete (Dremin D.A.)

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,20 +16,17 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.data = None
+        self.stack = []
+        self.top = None
 
-    def push(self, data):
+    def push(self, data) -> None:
         """
         Метод для добавления элемента на вершину стека
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        next_node = self.top
+        new_top = Node(data, next_node)
+        self.top = new_top
 
-    def pop(self):
-        """
-        Метод для удаления элемента с вершины стека и его возвращения
-
-        :return: данные удаленного элемента
-        """
-        pass
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push(self):
+        self.stack.push(1)
+        self.assertEqual(self.stack.top.data, 1)
